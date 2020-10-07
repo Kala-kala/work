e816ffc23133abdc6ad934406de10eb329e238db
Harshit Mohan <53287643+harshitmohan02@users.noreply.github.com>
2020-10-07 17:49:45 +0530

    Update compute.py

--- compute.py
+++ compute.py
@@ -4,7 +4,7 @@ def average(a, b, c, d):
     return 0.25*(a + b + c + d)
 
 def compute(a, b, c, d):
-    y = average(a, b, c, d);
+    y = Sum(a, b, c, d);
     return y
 
 def compute(a, b, c, d):
